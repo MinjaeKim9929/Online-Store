10cd9b5d1aaa93dd78986d00981891ce62a053c7
Minjae Kim <minjaekim9929@gmail.com>
2024-04-03 18:18:40 -0400

    Store item objects and image files added

--- script/script.js
+++ script/script.js
@@ -60,7 +60,7 @@ function initialize() {
 	const basketball = new storeItem(
 		'SPOR0001',
 		'NBA OFFICIAL GAME BASKETBALL',
-		199.99,
+		199.95,
 		70,
 		5,
 		'Sports & Outdoors',
@@ -156,5 +156,172 @@ function initialize() {
 		'/img/KEZEF_necklace.jpg'
 	);
 
-	storeItemArr.push(basketball, adapter_USB_C_20W, the_VHC, utopia_pillows, owala_water_bottle, KEZEF_necklace);
+	const dumbbell_hand_weights_set = new storeItem(
+		'SPOR0003',
+		'Neoprene Coated Dumbbell Hand Weight Set',
+		23.83,
+		24,
+		3,
+		'Sports & Outdoors',
+		0.79,
+		new Array(),
+		`Set of 2 dumbbells for resistance training; each dumbbell weighs 5 pounds
+		Easy-grip neoprene coating for a secure hold
+		Printed weight number on each end cap and color-coded for quick identification
+		Hexagonal shape prevents dumbbells from rolling away
+		Ideal for fitness classes or at-home workout routines`,
+		'/img/hand_weights_set.jpg'
+	);
+
+	const APPLE_airtag = new storeItem(
+		'ELEC0002',
+		'Apple AirTag 4 Pack',
+		129.0,
+		241,
+		10,
+		'Electronics',
+		3.0,
+		new Array(),
+		`Keep track of and find your items alongside friends and devices in the Find My app
+		Simple one-tap setup instantly connects AirTag with your iPhone or iPad
+		Play a sound on the built-in speaker to help find your things, or just ask Siri for help
+		Precision Finding with Ultra Wideband technology leads you right to your nearby AirTag (on select iPhone models)
+		Find items further away with the help of hundreds of millions of Apple devices in the Find My network
+		Put AirTag into Lost Mode to be automatically notified when it's detected in the Find My network
+		All communication with the Find My network is anonymous and encrypted for privacy, Location data and history are never stored on AirTag`,
+		'/img/apple_airtag.jpg'
+	);
+
+	const first_100_words = new storeItem(
+		'BOOK0002',
+		'First 100 Words: A Padded Board Book',
+		5.99,
+		65,
+		3,
+		'Books',
+		0.15,
+		new Array(),
+		`Your little one will soon learn some essential first words and pictures with this bright board book. There are 100 color photographs to look at and talk about, and 100 simple first words to read and learn, too. The pages are made from tough board for hours of fun reading, and the cover is softly padded for little hands to hold.`,
+		'/img/first_100_words.jpg'
+	);
+
+	const BRITA_standard_water_filter = new storeItem(
+		'HOME0002',
+		'Brita Standard Water Filter, 3 Count',
+		24.97,
+		90,
+		3,
+		'Home & Kitchen',
+		0,
+		new Array(),
+		`Get great-tasting water with Brita Standard water filter replacement; Packaging may vary. Switch to Brita and you can save money and replace 1800 single-use plastic bottles* a year *500 mL water bottle.
+		Reduce Chlorine (taste & odour) Mercury, Copper, Zinc, Cadmium.
+		For the best tasting water, replace your water filter after 151 litres or approximately every 2 months; Brita Standard filters last 2x longer than ZeroWater filters.
+		Compatible with all Brita pitchers and dispensers except Stream; Brita water filters are simple to replace with a pull top cap for easy removal`,
+		'/img/brita_standard_water_filter.jpg'
+	);
+
+	const BENSORTS_pillow_sandals = new storeItem(
+		'CLOT0002',
+		'BenSorts Women Men Pillow Sandals',
+		30.49,
+		99,
+		2,
+		'Clothing, Shoes & Accessories',
+		1.15,
+		new Array(),
+		`【Thickened Soft Sole EVA Foam】The pillow slippers use EVA soles and protection design to make you feel like stepping on the pillow. Forward and backward shock absorption, reduce foot damage.
+		【Double Anti-Slip】The interior of pillow slippers slides adopts a dot-shaped raised design, and the sole adopts a wave-shaped raised anti-skid design. Double anti-slip, double safety. More suitable for shower pool and beach wear.
+		【Occasion】 Indoor and outdoor, perfects for all season：house slippers, bathroom showers, swimming pool, SPA, sea beach ,leisure,street casual fashion, etc.
+		【ATTENTION】 This pillow sandals are made of EVA material, they may shrink and deform at high temperature, please do not expose them to direct sunlight for a long time.`,
+		'/img/BENSORTS_pillow_sandals.jpg'
+	);
+
+	const wiper_blade_26in = new storeItem(
+		'AUTO0001',
+		'Weatherbeater Wiper Blade, Black, 26 Inch',
+		12.97,
+		20,
+		1,
+		'Automotive',
+		0,
+		new Array(),
+		`Rain-X Weatherbeater are high-quality conventional wiper blades that have stood the test of time, meeting or exceeding OEM standards for quality and durability
+		The all natural rubber squeegee resists cracking, splitting, and tearing caused by hot and cold weather conditions, washer fluid, and road salt
+		Multiple pressure points and embedded friction reducers enable even pressure distribution across the length of the wiper blade helping to deliver a clean wipe
+		Conventional blade structure with a galvanized steel frame provides long-lasting strength and durability and prevents rust and corrosion
+		Weatherbeater wiper blades are equipped with a pre-installed J-hook adapter for easy installation on most vehicles and contains adapters for Bayonet and Pin Arms.`,
+		'/img/wiper_blade_26in.jpg'
+	);
+
+	const SHURE_SM58_LC = new storeItem(
+		'MUSI0001',
+		'Shure SM58-LC Cardioid Dynamic Vocal Microphone',
+		139.0,
+		55,
+		3,
+		'Musical Instrument, Stage & Studio',
+		0,
+		new Array(),
+		`EXEPTIONAL VOCAL PERFORMANCE - The SM58-LC delivers high-quality vocal reproduction, thanks to its tailored frequency response and brightened midrange for clear and present vocals.
+		INDUSTRY STANDARD - The first choice for performers around the globe, the SM58 Dynamic Vocal Microphone is ready to bring any performance to life. From the club to the stadium, Shure has your back.
+		CLARITY & CARE - The uniform cardioid polar pattern reproduces every note as it was intended, delivering warm, crisp sound time after time.
+		LEAVE BACKGROUND NOISE BEHIND - The SM58 has a spherical mesh grille and built-in pop filter to keep any unwanted noise out of the mix.
+		DROP THE MIC - The durable construction and break-resistant stand adapter makes the SM58 the perfect companion on the road, going strong through the toughest performances.
+		VERSATILE - The SM58 can hold its own live onstage as well as in the studio, isolating between vocals and instruments.
+		WHAT’S IN THE BOX - Shure SM58 Dynamic Vocal Microphone, 1 - A25D Swivel Stand Adapter, 1 - 95A2313 Storage Bag, Free 2-YEAR WARRANTY included.`,
+		'/img/SHURE_SM58_LC.jpg'
+	);
+
+	const FOCUSRITE_SCARLETT_2i2_Gen4 = new storeItem(
+		'MUSI0002',
+		'Focusrite Scarlett 2i2 4th Gen USB Audio Interface',
+		289.0,
+		9,
+		1,
+		'Musical Instrument, Stage & Studio',
+		10.89,
+		new Array(),
+		`The artist's interface: Scarlett's 4th Gen mic pres and a massive 120dB dynamic range deliver the audio performance you'd find in a pro studio.
+		Never lose a great take: Auto Gain sets the perfect level for your mic or guitar, and Clip Safe prevents clipping, so you can focus on the music.
+		Find your signature sound: Air mode lifts vocals and guitars to the front of the mix, adding musical presence and rich harmonic drive to your recordings.
+		Everything you need to record, mix and master your music: Includes industry-leading recording software and a full collection of record-making plugins.
+		Easy Start: It’s never been easier to get recording. Focusrite’s tried-and-tested Easy Start tool will have you making music in minutes.`,
+		'/img/SCARLETT_2i2_gen4.jpg'
+	);
+
+	const car_trashcan = new storeItem(
+		'AUTO0002',
+		'Car Trash Can',
+		18.99,
+		37,
+		2,
+		'Automotive',
+		0.99,
+		new Array(),
+		`【LARGE CAPACITY 】The size is 9.25*5.25*10.75 inches, our car trash can with a large compartment, two elastic mesh pockets on the side, and an open pocket at the front, which can hold 2.25 Gallon/8.5L capacity to save space, it can be folded up when not in use, very convenient.
+		【THOUGHTFUL DESIGN 】Our car trash bin with soft lid that is a rubber X-shaped opening and velcro seal can be loaded with tissue paper, fruit peels, and the bottle can be closed or even cover up the waste and keep the internal smell without lifting the lid.
+		【PREMIUM MATERIAL 】This car trash bag is made of waterproof and durable 600D high-quality material, the inner material of the car accessories women is a waterproof and easy-to-clean PEVA lining. You can put it directly in the garbage, or put a disposable bag inside, and use two side clips to fix the handle of the bag, just keep everything clean.
+		【MULTIPURPOSE 】PEVA lining also has the effect of thermal insulation. This car accessories not only hold garbage, but also a thermal insulation bag. You can put hot food, cold objects, fruits, drinks, snacks in it, and it can keep hot for several hours. The temperature of food and the coolness of drinks can also be used as a storage bag for other small items such as paper towels, toys, bags, snacks, umbrellas and other things you want to pack.
+		【FIT ANYWHERE IN CAR 】There is an easily adjustable buckle strap on the back of the car accessories, which can be placed in many places and can be hung on the front or back of the seat, the center console, the glove box and even the gear lever.`,
+		'/img/car_trashcan.jpg'
+	);
+
+	storeItemArr.push(
+		basketball,
+		adapter_USB_C_20W,
+		the_VHC,
+		utopia_pillows,
+		owala_water_bottle,
+		KEZEF_necklace,
+		dumbbell_hand_weights_set,
+		APPLE_airtag,
+		first_100_words,
+		BRITA_standard_water_filter,
+		BENSORTS_pillow_sandals,
+		wiper_blade_26in,
+		SHURE_SM58_LC,
+		FOCUSRITE_SCARLETT_2i2_Gen4,
+		car_trashcan
+	);
 }
